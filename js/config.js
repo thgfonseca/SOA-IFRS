1832f46d5b06796c08c82cc3e562629247643b33
thgfonseca <43590671+thgfonseca@users.noreply.github.com>
2026-03-22 20:25:15 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js — Configuração central
 const CONFIG = {
   // ← Cole aqui a URL do seu Apps Script após o deploy
-  API_URL: 'https://script.google.com/macros/s/SEU_ID_AQUI/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbztKFN0KVU_yaAa8vVicmU1r9sIc1xBl4CDE0i5l8n97u5rCALdAcHKwLrrvEANWYSa/exec'
 };
