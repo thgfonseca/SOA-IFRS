48729e7371ae2bf5c48217e1738d2aaa99dec595
thgfonseca <43590671+thgfonseca@users.noreply.github.com>
2026-03-22 22:24:10 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
 // js/config.js — Configuração central
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbztKFN0KVU_yaAa8vVicmU1r9sIc1xBl4CDE0i5l8n97u5rCALdAcHKwLrrvEANWYSa/exec',
-  GOOGLE_CLIENT_ID: '1051968147490-8p8vlivaumpq4eq400ru35u8o33sbopa.apps.googleusercontent.com'
+  API_URL: 'https://script.google.com/macros/s/AKfycbztKFN0KVU_yaAa8vVicmU1r9sIc1xBl4CDE0i5l8n97u5rCALdAcHKwLrrvEANWYSa/exec'
 };
-
